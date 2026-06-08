d32f647c3cf22cb5dbf4cada9acf8ff670ed2b49
PSM <chawin.suk4414@gmail.com>
2026-06-08 15:00:43 +0700

    Update color.py

--- color.py
+++ color.py
@@ -13,7 +13,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://psm67.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
